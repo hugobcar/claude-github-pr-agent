9e7735dffece09bf0a0aff7d520d22be95c270c7
Hugo Branquinho <hugobcar@hotmail.com>
2026-02-07 09:41:57 -0300

    chore: add a python file with bugs to test claude workflow

---
+++ index.py
@@ -0,0 +1,22 @@
+import bugsnag
+from bugsnag.flask import handle_exceptions
+import os
+
+# Configure bugsnag
+bugsnag.configure(
+  api_key=os.getenv("BUGSNAG_API_KEY")
+)
+
+# Attach bugsnag to Flask's exception handler
+app = Flask(__name__)
+handle_exceptions(app)
+
+@app.route("/get")
+def get():
+    return "Hello, World"
+
+@app.route("/bug")
+def get():
+    names = ("hugo")
+    names.append("bruno")
+    return "/bug"
